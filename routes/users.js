fc36d38a05573d2ef5a638e3d3cfaed3ce57fc5d
Eduardo <egsanche@espol.edu.ec>
2024-07-29 09:16:07 -0500

    Update

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+let crypto = require('crypto')
+
 const sequelize = require('../models/index.js').sequelize;
 var initModels = require("../models/init-models");
 var models = initModels(sequelize);
@@ -10,5 +12,21 @@ router.get('/', async function(req, res, next) {
   let usersCollection = await models.users.findAll({})
   res.render('crud', {title: 'CRUD with users', usersArray: usersCollection});
 });
+
+router.post('/', async (req, res) => {
+  let{ name, password, idrole } = req.body;
+
+  try{
+    let salt = process.env.SALT
+    let hash = crypto.createHmac('sha512', salt).update(password).digest("base64");
+    let passwordHash = salt + "$" + hash
+
+    let user = await models.users.create({name: name, password: passwordHash})
+
+    res.redirect('/users')
+  } catch(error){
+    res.status(400).send(error)
+  }
+})
 
 module.exports = router;
